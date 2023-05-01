6a2c4ef48fa989d37733470eecf9b364795215c2
mofnva <moblold@yahoo.co.jp>
2023-05-01 19:49:41 +0900

    attemptの参照内容が判明したのでデータ型を変更したが深い階層でエラー

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,6 +41,7 @@ class LoginController extends Controller
 
     public function login(Request $request){//メソッドインジェクション
         if($request->isMethod('post')){//POST通信でデータが送られてきたら
+            $data = $request;
             // ログインが成功したら、トップページへ
             //↓ログイン条件は公開時には消すこと
             if(Auth::attempt($data)){
