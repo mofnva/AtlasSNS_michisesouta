30ac8757c8c51638304a50bb362b1dc30611ca73
mofnva <moblold@yahoo.co.jp>
2023-05-14 21:59:06 +0900

    検索フォーム設置

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,4 +13,7 @@ class UsersController extends Controller
     public function search(){
         return view('users.search');
     }
+    public function index(){
+        return view('users.search');
+    }
 }
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
+{!! Form::open(['url'=>'<!--ここに検索機能走らせるURLを入れる-->'])!!}
 
+{{ Form::label('ユーザー名検索') }}
+{{ Form::text('searchName',null,['class' => 'input','required','maxlength'=>255,'minlength'=>1]) }}
+
+{{ Form::submit('検索') }}
+
+{!! Form::close() !!}
 
 @endsection
