55eaf0b4b6457aedc0f00e5e600fe99fa4ea8468
mofnva <moblold@yahoo.co.jp>
2023-06-08 22:35:23 +0900

    投稿編集・削除迷走中

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,28 +15,36 @@
 <br>
 
 <?php
-  for($cnt=0;$cnt!=count($viewPosts);$cnt=$cnt+1){
-      $postobj=$viewPosts[$cnt];
-      $postarr = (array)$postobj;
-      echo "<div>
-      <p>".$postarr['post']."</p>";
-      if($postarr['user_id'] == Auth::id()){
+ // for($cnt=0;$cnt!=count($viewPosts);$cnt=$cnt+1){
+      //$postobj=$viewPosts[$cnt];
+      //$postarr = (array)$postobj;
+      //echo "<div>
+      //<p>".$postarr['post']."</p>";
+      //if($postarr['user_id'] == Auth::id()){
 
-        echo "<form action=\"/delete\"method=\"post\" name=\"delete".$postarr['id']."\">
-          <script>
-          function delete".$postarr['id']."(){confirm(\"本当に削除しますか？\");
-          }
-          </script>
-          "?>
-        @csrf
-        <?php
-        echo "<input type=\"hidden\" value=".$postarr['id']." name=\"deleteId\">
-        <input type=\"submit\" onclick=\"delete".$postarr['id']."()\">
-        </form>";
-      };
-      echo "</div>
-      <br>";
-    };
-?>
+       // echo "<form action=\"/delete\"method=\"post\" name=\"delete".$postarr['id']."\">
+         // <script>
+         // function delete".$postarr['id']."(){confirm(\"本当に削除しますか？\");
+         // }
+          //</script>
+          //"
+        //echo "<input type=\"hidden\" value=".$postarr['id']." name=\"deleteId\">
+        //<input type=\"submit\" onclick=\"delete".$postarr['id']."()\">
+        //</form>";
+        ?>
+@for ($cnt=0;$cnt=count($viewPosts);$cnt=$cnt+1)
+{{
+  $postobj=$viewPosts[$cnt];
+      $postarr = (array)$postobj;
+{!! Form::open(['url'=>'/delete' 'method'=>'post'])!!}
+
+{{ Form::label($postarr['post']) }}
+{{ Form::text('searchName',null,['class' => 'input','required','maxlength'=>255,'minlength'=>1]) }}
+
+{{ Form::submit('検索') }}
+
+{!! Form::close() !!}
+<br>
+}}
 
 @endsection
