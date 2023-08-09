1a9767872e4e755940390ecf4bb0ce62818747e1
mofnva <moblold@yahoo.co.jp>
2023-08-09 15:52:45 +0900

    質問用

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,16 +7,16 @@
 <h2>新規ユーザー登録</h2>
 
 {{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
+{{ Form::text('username',null,['class' => 'input','maxlength'=>12,'minlength'=>2]) }}
 
 {{ Form::label('メールアドレス') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
+{{ Form::text('mail',null,['class' => 'input','maxlength'=>40,'minlength'=>5]) }}
 
 {{ Form::label('パスワード') }}
-{{ Form::text('password',null,['class' => 'input']) }}
+{{ Form::text('password',null,['class' => 'input','maxlength'=>20,'minlength'=>8]) }}
 
 {{ Form::label('パスワード確認') }}
-{{ Form::text('password-confirm',null,['class' => 'input']) }}
+{{ Form::text('password-confirm',null,['class' => 'input','maxlength'=>12,'minlength'=>2,'confirmed'=>'password']) }}
 
 {{ Form::submit('登録') }}
 
